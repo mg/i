6c400f8492db97c3b02974c4620ba0120fa6ea13
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2013-09-11 22:24:32 +0000

    i.Map: wrapped version

--- map.go
+++ map.go
@@ -4,25 +4,25 @@ package i
 type MapFunc func(Iterator) interface{}
 
 type imap struct {
-	wforward
+	WForward
 	fmap MapFunc
 	val  interface{}
 }
 
 func Map(fmap MapFunc, itr Forward) Forward {
 	m := imap{fmap: fmap}
-	m.wforward = *(WrapForward(itr))
+	m.WForward = *(WrapForward(itr))
 	return &m
 }
 
 func (m *imap) Next() error {
 	m.val = nil
-	return m.wforward.Next()
+	return m.WForward.Next()
 }
 
 func (m *imap) Value() interface{} {
 	if m.val == nil {
-		m.val = m.fmap(&m.wforward)
+		m.val = m.fmap(&m.WForward)
 	}
 	return m.val
 }

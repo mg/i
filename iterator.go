8380c8c18d7f1d480e0a5b9751d8229e5889ce7c
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2013-09-12 20:50:03 +0000

    Iterator documentation

--- iterator.go
+++ iterator.go
@@ -23,37 +23,76 @@ package i
 
 type (
 	Iterator interface {
+		// Return value at current position in data.
 		Value() interface{}
+
+		// Return last error occurred.
 		Error() error
+
+		// Set last error
 		SetError(error)
 	}
 
 	Forward interface {
+		// Forward iterator is an Iterator.
 		Iterator
+
+		// Move to the next position in the data. Moving after the end
+		// is reached is an error.
 		Next() error
+
+		// Check if the iterator is beyond the end of the data. This is an
+		// invalid position in the data, e.g. fetching the value at the
+		// end is an error.
 		AtEnd() bool
 	}
 
 	BiDirectional interface {
+		// BiDirectional is a Forward iterator.
 		Forward
+
+		// Move to the previous position in the data. Moving once beyond the
+		// starting position is valid. Moveing after that is an error.
 		Prev() error
+
+		// Check if the iterator is at the start of the data. This is a valid
+		// position in the data, e.g. fetching the value will return the first
+		// value.
 		AtStart() bool
 	}
 
 	BoundedAtStart interface {
+		// BoundedAtStart is a Forward iterator.
 		Forward
+
+		// Jump to the first position in the stream. This is a valid position
+		// in the stream. At this position AtStart() returns true.
 		First() error
 	}
 
 	Bounded interface {
+		// Bounded is a BiDirectional iterator.
 		BiDirectional
+
+		// Jump to the first position in the stream. This is a valid position
+		// in the stream. At this position AtStart() returns true.
 		First() error
+
+		// jump to the last position in the stream. This is a valid position
+		// in the stream. At this position AtEnd() returns false.
 		Last() error
 	}
 
 	RandomAccess interface {
+		// RandomAccess is a Bounded iterator.
 		Bounded
+
+		// Jump to any position in the stream. If the position is less than
+		// First position or greater than Last position the method returns
+		// an error.
 		Goto(int) error
+
+		// Get the length of the data stream.
 		Len() int
 	}
 )

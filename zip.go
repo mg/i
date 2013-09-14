a29b191780c0bd606a2dde436b821ce68526d7db
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2013-09-14 14:55:32 +0000

    i.Zip

---
+++ zip.go
@@ -0,0 +1,116 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2013 Magnús Örn Gylfason
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package i
+
+import "fmt"
+
+type zip struct {
+	itrs  []Forward
+	err   error
+	atEnd bool
+}
+
+func Zip(itrs ...Forward) Forward {
+	return &zip{itrs: itrs}
+}
+
+func (z *zip) Error() error {
+	return z.err
+}
+
+func (z *zip) SetError(err error) {
+	z.err = err
+}
+
+func (z *zip) Value() interface{} {
+	if z.atEnd {
+		z.err = fmt.Errorf("Calling Value() at end")
+		return nil
+	}
+	ret := make([]interface{}, len(z.itrs))
+	for idx, itr := range z.itrs {
+		ret[idx] = itr.Value()
+	}
+	return ret
+}
+
+func (z *zip) Next() error {
+	if z.atEnd {
+		z.err = fmt.Errorf("Calling Next() at end")
+		return z.err
+	}
+	for _, itr := range z.itrs {
+		if !itr.AtEnd() {
+			err := itr.Next()
+			if err != nil {
+				z.err = err
+				break
+			}
+		}
+	}
+	return z.err
+}
+
+func (z *zip) AtEnd() bool {
+	for _, itr := range z.itrs {
+		if itr.AtEnd() {
+			z.atEnd = true
+			return true
+		}
+	}
+	return false
+}
+
+type ziplongest struct {
+	zip
+}
+
+func ZipLongest(itrs ...Forward) Forward {
+	return &ziplongest{zip{itrs: itrs}}
+}
+
+func (z *ziplongest) Value() interface{} {
+	if z.atEnd {
+		z.err = fmt.Errorf("Calling Value() at end")
+		return nil
+	}
+	ret := make([]interface{}, len(z.itrs))
+	for idx, itr := range z.itrs {
+		if !itr.AtEnd() {
+			ret[idx] = itr.Value()
+		} else {
+			ret[idx] = nil
+		}
+	}
+	return ret
+}
+
+func (z *ziplongest) AtEnd() bool {
+	atEndCount := 0
+	for _, itr := range z.itrs {
+		if itr.AtEnd() {
+			atEndCount++
+		}
+	}
+	z.atEnd = atEndCount == len(z.itrs)
+	return z.atEnd
+}
